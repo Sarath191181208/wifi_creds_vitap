399ba242bda6a5b26590e8ac976d9124e06bc05c
Sarath191181208 <vssarathc04@gmail.com>
2024-04-12 14:31:36 +0530

    Created an extension

---
+++ js/login.js
@@ -0,0 +1,5 @@
+let ele = document.getElementById("ft_un");
+let etxt = `
+<input name="username" id="ft_un" type="text" autocorrect="off" autocapitalize="off" />
+`;
+ele.parentElement.innerHTML = etxt;
